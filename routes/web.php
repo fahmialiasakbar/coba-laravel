7871b2334cec3cd2c34d342217fac1b88dfcac7a
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-01-29 10:25:26 +0700

    3 - Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Fahmi Akbar',
+        'email' => 'fahmialiasakbar@gmail.com',
+        'image' => 'img/gw.jpg'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
